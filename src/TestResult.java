5cbc06827e7df813803a10fe88da3a82bf353fd8
Приходько Софія <sofiykapiryet@gmail.com>
2026-02-26 18:58:00 +0200

    done

--- src/TestResult.java
+++ src/TestResult.java
@@ -2,10 +2,10 @@ public class TestResult {
 
     public static void main(String[] args) {
 
-        int start = 10; 
-        int end = 50; 
-        
-        String primesList = Exercise.Calculate(start, end);
+        int start = 10;
+        int end = 50;
+
+        String primesList = Exercise.calculate(start, end);
         System.out.println("Прості числа між " + start + " та " + end + ": " + primesList);
     }
 }
