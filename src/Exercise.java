744c8171e1bf214f5ad07f895befc93c058ee8a0
Приходько Софія <sofiykapiryet@gmail.com>
2026-02-26 18:27:07 +0200

    done

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,26 @@
-//package domain;
+// package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    public static String Calculate(int start, int end) {
+        StringBuilder primes = new StringBuilder();
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                if (primes.length() > 0) {
+                    primes.append(", ");
+                }
+                primes.append(i);
+            }
+        }
+        return primes.toString();
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 }
